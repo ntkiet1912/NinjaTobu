191b59e5dbe0dad1ba55b784a8e8e8b0a51c170c
ntkiet1912 <pupgkiet@gmail.com>
2025-02-20 00:35:23 +0700

    update spike

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -5,7 +5,7 @@ public class Spike : MonoBehaviour
 {
     public Transform cameraTransform;
     private float previousCameraY;
-    public GameObject gameOverCanvas;
+
 
     private void Start()
     {
@@ -16,13 +16,5 @@ public class Spike : MonoBehaviour
         float deltaY = cameraTransform.position.y - previousCameraY;
         transform.position += new Vector3(0, deltaY, 0);
         previousCameraY = cameraTransform.position.y;
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Time.timeScale = 0;
-            gameOverCanvas.SetActive(true);
-        }
     }
 }
